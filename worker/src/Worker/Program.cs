024aa6687d3d3c8231771539f7c6e0512a581d4d
Grayson Adkins <grayson@idyl.tech>
2022-08-19 11:03:00 -0500

    revert changes

--- worker/src/Worker/Program.cs
+++ worker/src/Worker/Program.cs
@@ -16,14 +16,21 @@ namespace Worker
         {
             try
             {
-                var pghost = Environment.GetEnvironmentVariable("PGHOST") ?? "localhost";
-                var pguser = Environment.GetEnvironmentVariable("PGUSER") ?? "postgres";
-                var pgpassword = Environment.GetEnvironmentVariable("PGPASSWORD") ?? "postgres";
+                var pghost =
+                    Environment.GetEnvironmentVariable("PGHOST") ?? "localhost";
+                var pguser =
+                    Environment.GetEnvironmentVariable("PGUSER") ?? "postgres";
+                var pgpassword =
+                    Environment.GetEnvironmentVariable("PGPASSWORD")
+                        ?? "postgres";
 
-                var pgConnString = $"Server={pghost};Username={pguser};Password={pgpassword};";
+                var pgConnString =
+                    $"Server={pghost};Username={pguser};Password={pgpassword};";
                 var pgsql = OpenDbConnection(pgConnString);
 
-                var redisHost = Environment.GetEnvironmentVariable("REDIS_HOST") ?? "localhost";
+                var redisHost =
+                    Environment.GetEnvironmentVariable("REDIS_HOST")
+                        ?? "localhost";
                 var redisConn = OpenRedisConnection(redisHost);
                 var redis = redisConn.GetDatabase();
 
@@ -39,7 +46,8 @@ namespace Worker
                     Thread.Sleep(100);
 
                     // Reconnect redis if down
-                    if (redisConn == null || !redisConn.IsConnected) {
+                    if (redisConn == null || !redisConn.IsConnected)
+                    {
                         Console.WriteLine("Reconnecting Redis");
                         redisConn = OpenRedisConnection("localhost");
                         redis = redisConn.GetDatabase();
@@ -47,21 +55,27 @@ namespace Worker
                     string json = redis.ListLeftPopAsync("votes").Result;
                     if (json != null)
                     {
-                        var vote = JsonConvert.DeserializeAnonymousType(json, definition);
-                        Console.WriteLine($"Processing vote for '{vote.vote}' by '{vote.voter_id}'");
+                        var vote =
+                            JsonConvert
+                                .DeserializeAnonymousType(json, definition);
+                        Console
+                            .WriteLine($"Processing vote for '{vote.vote}' by '{
+                                vote.voter_id}'");
+
                         // Reconnect DB if down
-                        if (!pgsql.State.Equals(System.Data.ConnectionState.Open))
+                        if (
+                            !pgsql
+                                .State
+                                .Equals(System.Data.ConnectionState.Open)
+                        )
                         {
                             Console.WriteLine("Reconnecting DB");
                             pgsql = OpenDbConnection(pgConnString);
                         }
                         else
-                        { // Normal +1 vote requested
+                        {
+                            // Normal +1 vote requested
                             UpdateVote(pgsql, vote.voter_id, vote.vote);
-                            if (vote.vote == "b") // dogs vote twice.
-			                {
-                                UpdateVote(pgsql, vote.voter_id + "2", vote.vote);
-			                }
                         }
                     }
                     else
@@ -77,7 +91,8 @@ namespace Worker
             }
         }
 
-        private static NpgsqlConnection OpenDbConnection(string connectionString)
+        private static NpgsqlConnection
+        OpenDbConnection(string connectionString)
         {
             NpgsqlConnection connection;
 
@@ -104,14 +119,17 @@ namespace Worker
             Console.Error.WriteLine("Connected to db");
 
             var command = connection.CreateCommand();
-            command.CommandText = @"CREATE TABLE IF NOT EXISTS votes (
+            command.CommandText =
+                @"CREATE TABLE IF NOT EXISTS votes (
                                         id VARCHAR(255) NOT NULL UNIQUE,
                                         vote VARCHAR(255) NOT NULL
                                     )";
             command.ExecuteNonQuery();
 
             // Seed database with example votes.
-            using (var cmd = new NpgsqlCommand(@"INSERT INTO votes (id, vote) VALUES
+            using (
+                var cmd =
+                    new NpgsqlCommand(@"INSERT INTO votes (id, vote) VALUES
                                     ('fake1', 'a'),
                                     ('fake2', 'a'),
                                     ('fake3', 'a'),
@@ -127,22 +145,25 @@ namespace Worker
                                     ('fake13', 'b'),
                                     ('fake14', 'b'),
                                     ('fake15', 'b')
-                                    ", connection))
+                                    ",
+                        connection)
+            )
             {
-                try {
-                        cmd.ExecuteNonQuery();
+                try
+                {
+                    cmd.ExecuteNonQuery();
                 }
                 catch (PostgresException)
                 {
-                        Console.Error.WriteLine("db already seeded");
+                    Console.Error.WriteLine("db already seeded");
                 }
-
             }
 
             return connection;
         }
 
-        private static ConnectionMultiplexer OpenRedisConnection(string hostname)
+        private static ConnectionMultiplexer
+        OpenRedisConnection(string hostname)
         {
             // Use IP address to workaround https://github.com/StackExchange/StackExchange.Redis/issues/410
             var ipAddress = GetIp(hostname);
@@ -163,26 +184,33 @@ namespace Worker
             }
         }
 
-        private static string GetIp(string hostname)
-            => Dns.GetHostEntryAsync(hostname)
+        private static string GetIp(string hostname) =>
+            Dns
+                .GetHostEntryAsync(hostname)
                 .Result
                 .AddressList
                 .First(a => a.AddressFamily == AddressFamily.InterNetwork)
                 .ToString();
 
-        private static void UpdateVote(NpgsqlConnection connection, string voterId, string vote)
+        private static void UpdateVote(
+            NpgsqlConnection connection,
+            string voterId,
+            string vote
+        )
         {
             var command = connection.CreateCommand();
             try
             {
-                command.CommandText = "INSERT INTO votes (id, vote) VALUES (@id, @vote)";
+                command.CommandText =
+                    "INSERT INTO votes (id, vote) VALUES (@id, @vote)";
                 command.Parameters.AddWithValue("@id", voterId);
                 command.Parameters.AddWithValue("@vote", vote);
                 command.ExecuteNonQuery();
             }
             catch (DbException)
             {
-                command.CommandText = "UPDATE votes SET vote = @vote WHERE id = @id";
+                command.CommandText =
+                    "UPDATE votes SET vote = @vote WHERE id = @id";
                 command.ExecuteNonQuery();
             }
             finally
